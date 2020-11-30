dd83babc683511e007740993ee126cb98b4cffb0
ww243 <2500494776@qq.com>
2020-11-30 20:29:15 +0800

    第一次

---
+++ src/main/java/com/baizhi/git_yingx/GitYingxApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_yingx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitYingxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitYingxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/git_yingx/GitYingxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_yingx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitYingxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
