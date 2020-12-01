d8bc01b798bf9b5c01685c4f08fa01a793251797
ww243 <2500494776@qq.com>
2020-12-01 18:50:09 +0800

    用户:小黑！

---
+++ src/main/java/com/baizhi/entity/File.java
@@ -0,0 +1,23 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author:ljn
+ * @Description:
+ * @Date:2020/12/01 18:47
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class File implements Serializable {
+
+    private String id;
+    private String name;
+    private String img;
+
+}
