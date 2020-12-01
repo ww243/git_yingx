49c899b91feb76ea3ba0c2a8d78e6f510de56dfb
ww243 <2500494776@qq.com>
2020-12-01 19:08:37 +0800

    用户:查所有

---
+++ src/main/java/com/baizhi/dao/AdminDao.java
@@ -0,0 +1,16 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Admin;
+
+import java.util.List;
+
+/**
+ * @author:ljn
+ * @Description:
+ * @Date:2020/12/01 19:02
+ */
+public interface AdminDao {
+
+    List<Admin> selectAll();
+
+}
